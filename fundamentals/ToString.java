445f2a8c4d60ceeab2400684374ace4bf2535f1a
Computer Science and Applied Engineering Academy <mharrell@hcvsd.org>
2025-10-14 13:05:57 +0000

    adds comments to ToString lesson

--- fundamentals/ToString.java
+++ fundamentals/ToString.java
@@ -1,28 +1,36 @@
+// Java's built in toString() method returns the memory address of an object. 
+// It is automatically called when we, for example, try to print the name of an object instance.   
+
 class Car {
+
     String model;
-    int price;
-
+    int year;
+        
+    // This custom method toString() overrides Java's built-in toString() method. 
+    // Delete or comment-out this toString() method to see Java's built-in method at work. 
     public String toString() {
-        return model + ":" + price;
+        return year + " " + model;
     }
 
-    }
+}
 
 public class ToString {
 
-    public static void main(String a[]) {
+    public static void main(String[] args) {
 
         Car obj1 = new Car();
         obj1.model = "Nucleon";
-        obj1.price = 10000000;
+        obj1.year = 1957;
 
         Car obj2 = new Car();
-        obj2.model = "Nucleon0";
-        obj2.price = 10000;
+        obj2.model = "Fulgur";
+        obj2.year = 1958;
 
-        System.out.println(obj1.toString());
-        System.out.println(obj2.toString());
+        System.out.println(obj1.toString()); 
+        System.out.println(obj2); 
+        //returns Car@3d012ddd and Car@626b2d4a with Java's built-in method (your hex values will be different).
+        //returns the object's values when we override toString() with our own method in the Car class. 
+
+        //Note: you can actually delete the "toString()" called in this class (like I did for obj2). Java will automatically call it anyway.
     }
 }    
-
-
