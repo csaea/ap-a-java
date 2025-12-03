21cfb67e76c6f89302d624b2479b701a9b2309ee
Michael Harrell <183651098+csaea@users.noreply.github.com>
2025-12-03 13:29:01 +0000

    First commit

---
+++ DataTypes.java
@@ -0,0 +1,16 @@
+// Java is a STRONGLY TYPED language; you have to DEFINE/DECLARE a variable
+// you have to declare its DATA TYPE
+
+class DataTypeExamples {
+        int myInt = 42;
+        double myDouble = 189.75;
+        char myChar = 'Q';
+        boolean myBool = false;
+}
+
+public class DataTypes {
+    public static void main(String[] args) {
+         DataTypeExamples dt = new DataTypeExamples();     
+         System.out.println("myInt: " + dt.myInt);
+    }
+}
