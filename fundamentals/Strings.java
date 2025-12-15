7b3e83a1ac0caf7e10cffdef05ccab9cb8b90dac
Michael Harrell <183651098+csaea@users.noreply.github.com>
2025-12-15 14:16:20 +0000

    updates strings lesson

--- fundamentals/Strings.java
+++ fundamentals/Strings.java
@@ -1,41 +1,44 @@
 
 // In Java, strings are not primitive data types! A String is a class that 'strings togeter' a bunch of characters.
-// They are their own class, with many built-in methods for string manipulation.
+// They are their own class, with LOTS of built-in methods for string manipulation.
 // "String literal" refers to any of the characters set in quotation marks. 
-// NEVER use double equals (==) to compare strings. Always use .equals()
 // Link to unicode chart: https://www.ssec.wisc.edu/~tomw/java/unicode.html#x0000
 
 public class Strings {
 
     public static void main(String[] args) {
 
-        // Strings are object classes. We can make a string with normal object construction like this:
-        String nameObj = new String("Didi"); //String as whole new object. (We rarely use this). 
+        // Java Strings are an object made of concatenated characters. 
+        // Each character in a STRING LITERAL has an index:
 
-        // 0 1 2 3
-        // D i d i
+        //   0 1 2 3
+        // " D i d i "
 
-        //BUT it's more common to simply use STRING LITERALS like this:
+        // We can make a string with normal object construction like this:
+        String nameObj = new String("Didi"); 
+
+        //But it's more common to simply use the STRING LITERALS like this:
         String name1 = "Didi";
         String name2 = "Gogo";
         String name3 = "Gogo"; 
 
-        System.out.println("name1.equals(nameObj): " + name1.equals(nameObj)); //false because we are comparing two different objects in memory.
-        System.out.println("name1.equals(name2): " + name1.equals(name2));
-        System.out.println("name2.equals(name3): " + name2.equals(name3));
+        System.out.println("\nUsing == for same values: " + (name1 == nameObj)); //Both "Didi" but returns false!? because we are comparing two different objects in memory.
+        System.out.println("Using .equals() for diff value: " + name1.equals(name2)); // Returns false
+        System.out.println("Using .equals() for same  values: " + name2.equals(name3)); // Returns true
 
-        // BEWARE, never use == to :
-        System.out.println("name1 == nameObj: " + (name1 == nameObj));  // NEVER DO THIS. == is for numbers, this is comparing memory address, so this is false.
+        // NEVER use double equals (==) to compare strings. Always use .equals()
+        System.out.println("name1 == nameObj: " + (name1 == nameObj));  // == is primarily for numbers
         System.out.println("name2 == name3: " + (name2 == name3)); // Even though it works in some cases, like here (string pool/ref), but not all (objs). 
 
-        // compareTo for comparing lexicography (alphabetically) in UNICODE, uppercase before lowercase
-        System.out.println("Compare: " + name1.compareTo(name2)); // returns -3 because Didi comes 3 letters before Gogo
+        // compareTo for comparing lexicography (alphabetically) in UNICODE. Uppercase letters come before lowercase
+        // Link to unicode chart: https://www.ssec.wisc.edu/~tomw/java/unicode.html#x0000
+        System.out.println("\nCompare: " + name1.compareTo(name2)); // returns -3 because Didi comes 3 letters before Gogo
 
         // CONCATENATION
         String friends = name1 + " and " + name2;
         System.out.println("Concatenation: " + friends);  
 
-        // HELPFUL BUILT-IN METHODS
+        // OTHER HELPFUL BUILT-IN METHODS
         System.out.println("# of chars in string: " + name1.length());
         System.out.println("Index select: " + name2.indexOf("o")); //Experiment with this
         System.out.println("Character select: " + name2.charAt(2));
