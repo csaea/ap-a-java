4c40e109ca31b644264641062660ca31b29bde0a
Michael Harrell <183651098+csaea@users.noreply.github.com>
2026-02-17 14:06:51 +0000

    updates getset class

--- fundamentals/GetSetTester.java
+++ fundamentals/GetSetTester.java
@@ -1,14 +1,16 @@
+// ENCAPSULATION and Getters and Setters
+// Read: https://www.w3schools.com/java/java_encapsulation.asp
+
 public class GetSetTester {
     public static void main(String[] args) {
 
         //create a new object for my GetSet class
         GetSet name = new GetSet();
 
-        System.out.println(name.getUsername());
-        name.setUsername("pineappleghost333");
-
-        System.out.println(name.getUsername());
-
+        System.out.println(name.getUsername()); //displays default username value of null
+        name.setUsername("pineappleghost333"); //sets (or mutates) username to new value
+        System.out.println(name.getUsername()); //displays new username 
+        
     }
 }
 
@@ -20,7 +22,8 @@ class GetSet{
     public String getUsername(){
         return username;
     }
-
+    
+    //mutator methods aka SETTERS
     public void setUsername(String newUsername){
         this.username = newUsername;
     }
