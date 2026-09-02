db6c40b3ba49e8217d0a2ba67c1cddaff3f5ab59
Michael Harrell <183651098+csaea@users.noreply.github.com>
2026-09-02 13:20:50 +0000

    crashcourse

--- CrashCourse/CrashCourse.java
+++ CrashCourse/CrashCourse.java
@@ -1,3 +1,7 @@
+package CrashCourse;
+// If your Java file isn't running, you *might* need to add the following line and uncomment it, at the top of your file: 
+// package CrashCourse (the name of the folder that includes CrashCourse.java)
+
 public class CrashCourse {     // class header
 
     public static void main(String[] args) {
@@ -9,35 +13,36 @@ public class CrashCourse {     // class header
          Here
          */
 
-         System.out.print("Hello");
-         System.out.println("World"); // auto-prints on a new line
-         System.out.println("Hello world");
+        System.out.print("Hello");
+        System.out.println("World"); // prints on a new line
+        System.out.println("Hello world");
 
-         // varialbe declarations:
-         // PRIMITIVE VARIABLES
-         int a; // integer
-         double b; // decimal, or float
-         boolean c; // true or false;
+        // VARIABLE DECLARATIONS:
+        // primitive variables:
 
-         a = 4;
-         b = 5.5;
-         c = false; // lowercase
+        int a; // integer
+        double b; // decimal, or float
+        boolean c; // true or false;
+        
+        a = 4;
+        b = 5.5;
+        c = false; // lowercase
 
-         // Arithmetic Operators 
-         // + - / *  %
-         // += -= /=  %=
+        // Arithmetic Operators 
+        // + - / *  %
+        // += -= /=  %=  (compound assignment operators)
 
-        int d = 3;
-        d += 7;
+        int d = 3 - 1;
+        d += 7; // => 9
 
         System.out.println("d is equal to " + d);
 
         // increment and decrement by one:
         // --  ++
-        d--;
-        d++;
-        d++;
-        System.out.println("d is NOW equal to " + d);
+        d--; // 9 - 1 = 8
+        d--; // 8 - 1 = 7
+        d++; // 7 + 1 = 8
+        System.out.println("d is NOW equal to " + d); // => 8
 
         //COMPARISONS (always returns true or false)
         //  <  >   <=   >=   ==   !=
